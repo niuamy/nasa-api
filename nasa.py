5ada0ec15d8cac31d5215a266965c93d148a853a
Emily Metzger <emilycecilia19@gmail.com>
2023-01-10 18:02:03 -0500

    trying to commit

--- nasa.py
+++ nasa.py
@@ -3,16 +3,37 @@ from urllib.request import urlretrieve
 
 apiKey = 'DEMO_KEY'
 
-def getPic():
+def chooseDate():
+    date = input("Enter the date you would like an image from in YYYY-MM-DD format: ")
+    return date
+
+
+def printResults(response, date):
+    link = response['url']
+    explanation = response['explanation']
+    title = response['title']
+
+    print(f"On {date}, NASA's astronomy photo of the day was entitled \"{title}\".")
+    print(f"\nAccompanied with the photo is the explanation: \"{explanation}\"")
+    print(f"\nThe image can be seen here: {link}")
+
+
+
+def getPic(date):
   URL_APOD = "https://api.nasa.gov/planetary/apod"
-  date = '2020-01-22'
   params = {
       'api_key': apiKey,
       'date': date,
       'hd': 'True'
   }
   response = requests.get(URL_APOD, params=params).json()
-  print(response)
+  printResults(response, date)
 
 
-getPic()
+def main():
+    date = chooseDate()
+    getPic(date)
+
+
+if __name__ == "__main__":
+    main()
